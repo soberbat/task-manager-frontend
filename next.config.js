e5f15b351d782a3149a2825189993e7db2ec7100
Berat Genç <beratgenc@Berats-MacBook-Pro.local>
2024-01-18 12:55:12 +0300

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
-      }
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["~"] = path.join(__dirname, "src");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
