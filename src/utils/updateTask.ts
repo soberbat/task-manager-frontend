305fed08f4b63a7b3d2ddb8c0c2e10e1411b95d5
Berat Genç <beratgenc@berats-mbp.local>
2024-01-21 14:12:33 +0300

    updated url

--- src/utils/updateTask.ts
+++ src/utils/updateTask.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import axiosConfig from "./axios.config";
 
 export default async (taskData: {}, taskId: number) => {
-  const productionUrl = `https://api.taskermanager.online/project/${taskId}`;
+  const productionUrl = `https://api.taskermanager.online/tasks/${taskId}`;
   const devUrl = `http://localhost:3001/tasks/${taskId}`;
   try {
     const response = await axios.patch(productionUrl, taskData, axiosConfig);
